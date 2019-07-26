ecfeaa929bcb935ea48f9c82e38f6ef9854f273c
Bharathan Kumaran <reachbharathan@gmail.com>
2019-07-26 11:18:46 +0800

    [Bharathan] update gatling gradle tasks

---
+++ src/test/java/features/EmployeeSimulation.scala
@@ -0,0 +1,16 @@
+package features
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+
+import scala.concurrent.duration._
+
+class EmployeeSimulation extends Simulation {
+
+  val employee = scenario("employee").exec(karateFeature("classpath:features/Employee.feature"))
+
+  setUp(
+    employee.inject(rampUsers(10) during (5 seconds))
+  )
+
+}
